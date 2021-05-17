a1c2e5f1da3c7be21ae62b03141666db38044544
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-05-17 18:35:36 +0300

    get from DB

--- src/Profile.js
+++ src/Profile.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
 import Page404 from './Page404';
+import BestBooks from './BestBooks';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+
+      loading: false,
+    });
+  }
+
+
+
   render() {
+
     const { user } = this.props.auth0;
     return (
       <>
-        {(user !== undefined) ?
-          (
-            <>
-              <div>Hello {user.name}</div>
-              <div>Your Email: {user.email}</div>
-            </>
-          ) : (
-            <>
-              <Page404 />
-            </>
-          )}
+        {this.state.loading ? (
+          <>
+            loading...
+          </>
+        ) : (
+          <>
+            {(user !== undefined) ?
+              (
+                <>
+
+                  <div>Hello {user.name}</div>
+                  <div>Your Email: {user.email}</div>
+                  <BestBooks />
+
+                </>
+              ) : (
+                <>
+                  <Page404 />
+                </>
+              )}
+          </>)}
+
 
       </>);
   }
